bdcb8c64c3c6fed2db59bc6bce1e794c089bf182
sebasman <sebasman@stud.ntnu.no>
2019-01-08 12:15:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # HverdagsHelt - Team 3
 
-knuserare123 var her
+The application will reload on source changes.
 
+## Client: run tests and start
+
+From the top-level repository folder:
+
+```sh
+cd client
+npm install
+npm test
+npm start
+```
+
+## Server: run tests and start
+
+Prerequisite: mysql-server installed locally
+
+From the top-level repository folder:
+
+```sh
+cd server
+npm install
+npm test
+npm start
+```
+
+## Open application
+
+http://localhost:3000
---
+++ server/src/models.js
@@ -0,0 +1,47 @@
+// @flow
+
+import Sequelize from 'sequelize';
+import type { Model } from 'sequelize';
+
+let sequelize = new Sequelize(
+  process.env.CI ? 'articles' : 'sebasman',
+  process.env.CI ? 'root' : 'sebasman',
+  process.env.CI ? '' : 'GSBLuzbB',
+  {
+    host: process.env.CI ? 'mysql' : 'mysql.stud.iie.ntnu.no',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+export let Students: Class<
+  Model<{ id?: number, firstName: string, lastName: string, email: string }>
+> = sequelize.define('Students', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING
+});
+
+// Drop tables and create test data when not in production environment
+let production = process.env.NODE_ENV === 'production';
+// The sync promise can be used to wait for the database to be ready (for instance in your tests)
+export let sync = sequelize.sync({ force: production ? false : true }).then(() => {
+  if (!production)
+    return Students.create({
+      firstName: 'Ola',
+      lastName: 'Jensen',
+      email: 'ola.jensen@ntnu.no'
+    }).then(() =>
+      Students.create({
+        firstName: 'Kari',
+        lastName: 'Larsen',
+        email: 'kari.larsen@ntnu.no'
+      })
+    );
+});
