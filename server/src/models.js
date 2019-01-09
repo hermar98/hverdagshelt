d704e82458ca697287b8d5b678a234345b1e6b66
sebasman <sebasman@stud.ntnu.no>
2019-01-09 14:18:28 +0100

    formating

--- server/src/models.js
+++ server/src/models.js
@@ -19,32 +19,29 @@ let sequelize = new Sequelize(
   }
 );
 
-export let Students: Class<
-  Model<{ id?: number, firstName: string, lastName: string, email: string }>
-> = sequelize.define('Students', {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+export let User: Class<
+  Model<{
+    user_id?: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    rank: number,
+    salt: string,
+    hash_str: string
+  }>
+> = sequelize.define('User', {
+  user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   firstName: Sequelize.STRING,
   lastName: Sequelize.STRING,
-  email: Sequelize.STRING
+  email: Sequelize.STRING,
+  rank: Sequelize.INTEGER,
+  salt: Sequelize.STRING,
+  hash_str: Sequelize.STRING
 });
 
-export let User: Class<
-    Model<{ user_id?: number, firstName: string, lastName: string, email: string, rank: number, salt: string, hash_str: string }>
-    > = sequelize.define('User', {
-    user_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    firstName: Sequelize.STRING,
-    lastName: Sequelize.STRING,
-    email: Sequelize.STRING,
-    rank: Sequelize.INTEGER,
-    salt: Sequelize.STRING,
-    hash_str: Sequelize.STRING
-});
-
-export let County: Class<
-    Model<{ county_id?: number, name: string}>
-    > = sequelize.define('County', {
-    county_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    name: Sequelize.STRING
+export let County: Class<Model<{ county_id?: number, name: string }>> = sequelize.define('County', {
+  county_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: Sequelize.STRING
 });
 
 export let Municipal: Class<Model<{ mun_id?: number, name: string }>> = sequelize.define('Municipal', {
@@ -117,23 +114,23 @@ export let Event: Class<
   date: Sequelize.DATE
 });
 
-County.hasMany(Municipal, {foreignKey: 'county_id'});
-Municipal.hasMany(User, {foreignKey: 'municipal_id'});
-Municipal.hasMany(Issue, {foreignKey: 'municipal_id'});
+County.hasMany(Municipal, { foreignKey: 'county_id' });
+Municipal.hasMany(User, { foreignKey: 'municipal_id' });
+Municipal.hasMany(Issue, { foreignKey: 'municipal_id' });
 
-Municipal.belongsToMany(User, {through: 'User_municipal', foreignKey: 'mun_id'});
-User.belongsToMany(Municipal, {through: 'User_municipal', foreignKey: 'user_id'});
+Municipal.belongsToMany(User, { through: 'User_municipal', foreignKey: 'mun_id' });
+User.belongsToMany(Municipal, { through: 'User_municipal', foreignKey: 'user_id' });
 
-User.belongsToMany(Issue, {through: 'User_issue', foreignKey: 'user_id'});
-Issue.belongsToMany(User, {through: 'User_issue', foreignKey: 'issue_id'});
+User.belongsToMany(Issue, { through: 'User_issue', foreignKey: 'user_id' });
+Issue.belongsToMany(User, { through: 'User_issue', foreignKey: 'issue_id' });
 
-User.hasMany(Issue, {foreignKey: 'user_id'});
-User.hasMany(Event, {foreignKey: 'user_id'});
-User.hasMany(Feedback, {foreignKey: 'user_id'});
+User.hasMany(Issue, { foreignKey: 'user_id' });
+User.hasMany(Event, { foreignKey: 'user_id' });
+User.hasMany(Feedback, { foreignKey: 'user_id' });
 
-Issue.belongsTo(Issue_category, {foreignKey: 'category_id}'});
-Issue.belongsTo(Status, {foreignKey: 'status_id'});
-Feedback.hasMany(Issue, {foreignKey: 'feedback_id'});
+Issue.belongsTo(Issue_category, { foreignKey: 'category_id}' });
+Issue.belongsTo(Status, { foreignKey: 'status_id' });
+Feedback.hasMany(Issue, { foreignKey: 'feedback_id' });
 
 User.hasMany(Issue, { foreignKey: 'user_id' });
 User.belongsToMany(Issue, { through: 'UserIssue' });
@@ -157,113 +154,11 @@ let production = process.env.NODE_ENV === 'production';
 export let sync = sequelize.sync({ force: production ? false : true }).then(() => {
   if (!production)
     return User.create({
-        firstName: 'Vegard',
-        lastName: 'Andersson',
-        email: 'vegaande@ntnu.stud.no',
-        rank: 1,
-        salt: 'b79ryp97',
-        hash_str: '897dfjsodif5vx24c5vsldfskdclz97cyw7e3o2inJKHaospk902'
-    }).then(() =>
-      Students.create({
-        firstName: 'Kari',
-        lastName: 'Larsen',
-        email: 'kari.larsen@ntnu.no'
-      })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    ).then(() =>
-        Students.create({
-            firstName: 'Kari',
-            lastName: 'Larsen',
-            email: 'kari.larsen@ntnu.no'
-        })
-    );
+      firstName: 'Vegard',
+      lastName: 'Andersson',
+      email: 'vegaande@ntnu.stud.no',
+      rank: 1,
+      salt: 'b79ryp97',
+      hash_str: '897dfjsodif5vx24c5vsldfskdclz97cyw7e3o2inJKHaospk902'
+    });
 });
