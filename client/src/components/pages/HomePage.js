6368bd5d434af1c3534aebbfe07748b4630f7c72
Herman Christiansen <hermanchristiansen@dhcp-10-22-154-221.wlan.ntnu.no>
2019-01-09 13:47:02 +0100

    minor changes to homepage

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,19 +1,15 @@
 import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
-import { User } from '../../services';
 import { Alert, NavBar, Form, Card, Button } from '../../widgets';
 import Menu from '../../components/menu/Menu.js';
-import UploadImageButton from '../../components/image/UploadImageButton.js';
+/*import UploadImageButton from '../../components/image/UploadImageButton.js';*/
 
 export default class HomePage extends Component {
-  user = new User();
-
   render() {
     return (
       <div>
         <Menu />
-        <UploadImageButton />
       </div>
     );
   }
