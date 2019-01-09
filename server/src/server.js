dc556947bec3215839ed8dcc885960f487a8d57f
sebasman <sebasman@stud.ntnu.no>
2019-01-09 09:13:39 +0100

    minor changes

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import path from 'path';
 import reload from 'reload';
 import fs from 'fs';
-import { Students,Events } from './models.js';
+import { Students, Events } from './models.js';
 
 type Request = express$Request;
 type Response = express$Response;
@@ -31,8 +31,14 @@ app.get('/students/:id', (req: Request, res: Response) => {
 });
 
 app.get('/users/:id', (req: Request, res: Response) => {
-  return Case.findAll({ where: { user_id: Number(req.params.id) } }).then(student =>
-    student ? res.send(student) : res.sendStatus(404)
+  return User.findAll({ where: { user_id: Number(req.params.id) } }).then(user =>
+    user ? res.send(user) : res.sendStatus(404)
+  );
+});
+
+app.get('/users/:id/issue', (req: Request, res: Response) => {
+  return Issue.findAll({ where: { user_id: Number(req.params.id) } }).then(issue =>
+    issue ? res.send(issue) : res.sendStatus(404)
   );
 });
 
@@ -56,74 +62,67 @@ app.put('/students', (req: Request, res: Response) => {
 app.get('/events', (req: Request, res: Response) => {
   return Events.findAll().then(events => res.send(events));
 });
-app.get('/events/:id', (req:Request,res:Response) => {
-  return Events.findOne({where:{event_id: Number(req.params.id)}}).then(event =>
-      event ? res.send(event) : res.sendStatus(404)
+app.get('/events/:id', (req: Request, res: Response) => {
+  return Events.findOne({ where: { event_id: Number(req.params.id) } }).then(event =>
+    event ? res.send(event) : res.sendStatus(404)
   );
 });
 app.put('/events/:id', (req: Request, res: Response) => {
-  if(!(req.body instanceof Object)) return res.sendStatus(400);
+  if (!(req.body instanceof Object)) return res.sendStatus(400);
   return Events.update(
-      {
-          title: req.body.title,
-          content: req.body.content,
-          image: req.body.image,
-          longitude: req.body.longitude,
-          latitude: req.body.latitude,
-          time_start: req.body.time_start,
-          time_end: req.body.time_end
-      }, {
-        where: {
-          event_id: req.params.id
-        }
+    {
+      title: req.body.title,
+      content: req.body.content,
+      image: req.body.image,
+      longitude: req.body.longitude,
+      latitude: req.body.latitude,
+      time_start: req.body.time_start,
+      time_end: req.body.time_end
+    },
+    {
+      where: {
+        event_id: req.params.id
       }
-  ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)))
+    }
+  ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
-app.post('/events', (req:Request, res: Response) => {
-    if(!(req.body instanceof Object)) return res.sendStatus(400);
-    return Events.create(
-        {
-            title: req.body.title,
-            content: req.body.content,
-            image: req.body.image,
-            longitude: req.body.longitude,
-            latitude: req.body.latitude,
-            time_start: req.body.time_start,
-            time_end: req.body.time_end
-        }
-    ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)))
+app.post('/events', (req: Request, res: Response) => {
+  if (!(req.body instanceof Object)) return res.sendStatus(400);
+  return Events.create({
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+    longitude: req.body.longitude,
+    latitude: req.body.latitude,
+    time_start: req.body.time_start,
+    time_end: req.body.time_end
+  }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
-app.delete('/events/:id', function (req, res) {
-    return Event.destroy(
-        {
-            where: {
-              event_id: req.params.id
-            }
-        }
-        ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
+app.delete('/events/:id', function(req, res) {
+  return Event.destroy({
+    where: {
+      event_id: req.params.id
+    }
+  }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
 
-app.post('/issues', (req:Request, res: Response) => {
-    if(!(req.body instanceof Object)) return res.sendStatus(400);
-    return Issue.create(
-        {
-            title: req.body.title,
-            content: req.body.content,
-            image: req.body.image,
-            longitude: req.body.longitude,
-            latitude: req.body.latitude,
-            date: sequelize.literal("CURRENT_TIMESTAMP")
-        }
-    ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)))
+app.post('/issues', (req: Request, res: Response) => {
+  if (!(req.body instanceof Object)) return res.sendStatus(400);
+  return Issue.create({
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+    longitude: req.body.longitude,
+    latitude: req.body.latitude,
+    date: sequelize.literal('CURRENT_TIMESTAMP')
+  }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
-app.delete('/issues/:id', function (req, res) {
-    return Issue.destroy(
-        {
-            where: {
-              case_id: req.params.id
-            }
-        }
-        ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
+app.delete('/issues/:id', function(req, res) {
+  return Issue.destroy({
+    where: {
+      case_id: req.params.id
+    }
+  }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
 // Hot reload application when not in production environment
 if (process.env.NODE_ENV !== 'production') {
