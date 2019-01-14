5d9cb4d40bd0dc691a006dfbbb32b51e846776b1
sebasman <sebasman@stud.ntnu.no>
2019-01-14 12:43:15 +0100

    .

--- client/src/components/pages/ForgotPassword.js
+++ client/src/components/pages/ForgotPassword.js
@@ -12,124 +12,124 @@ const title = {
 };
 
 export class ForgotPassword extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      email: '',
-      showError: false,
-      messageFromServer: '',
-      showNullError: false
-    };
-  }
-
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value
-    });
-  };
-
-  sendEmail = e => {
-    e.preventDefault();
-    if (this.state.email === '') {
-      this.setState({
-        showError: false,
-        messageFromServer: '',
-        showNullError: true
-      });
-    } else {
-      axios
-        .post('http://localhost:3000/forgotPassword', {
-          email: this.state.email
-        })
-        .then(response => {
-          console.log(response.data);
-          if (response.data === 'email not in db') {
-            this.setState({
-              showError: true,
-              messageFromServer: '',
-              showNullError: false
-            });
-        } else {
-            axios
-                .post('http://localhost:3003/forgotPassword', {
-                    email: this.state.email,
-                })
-                .then(response => {
-                    console.log(response.data);
-                    if (response.data === 'email not in db') {
-                        this.setState({
-                            showError: true,
-                            messageFromServer: '',
-                            showNullError: false,
-                        });
-                    } else if (response.data === 'recovery email sent') {
-                        this.setState({
-                            showError: false,
-                            messageFromServer: 'recovery email sent',
-                            showNullError: false,
-                        });
-                    }
-                })
-                .catch(error => {
-                    console.log(error.data);
-                });
-        }
-    };
-
-    render() {
-        const { email, messageFromServer, showNullError, showError } = this.state;
-
-        return (
-            <div>
-                <Menu/>
-                <HeaderBar title={title} />
-                <form className="profile-form" onSubmit={this.sendEmail}>
-                    <TextField
-                        style={inputStyle}
-                        id="email"
-                        label="email"
-                        value={email}
-                        onChange={this.handleChange('email')}
-                        placeholder="Email Address"
-                    />
-                    <SubmitButtons
-                        buttonStyle={forgotButton}
-                        buttonText={'Send Password Reset Email'}
-                    />
-                </form>
-                {showNullError && (
-                    <div>
-                        <p>The email address cannot be null.</p>
-                    </div>
-                )}
-                {showError && (
-                    <div>
-                        <p>
-                            That email address isn't recognized. Please try again or register
-                            for a new account.
-                        </p>
-                        <LinkButtons
-                            buttonText={`Register`}
-                            buttonStyle={registerButton}
-                            link={'/register'}
-                        />
-                    </div>
-                )}
-                {messageFromServer === 'recovery email sent' && (
-                    <div>
-                        <h3>Password Reset Email Successfully Sent!</h3>
-                    </div>
-                )}
-                <LinkButtons
-                    buttonText={`Go Home`}
-                    buttonStyle={homeButton}
-                    link={'/'}
-                />
-            </div>
-        );
-    }
-  };
+  // constructor() {
+  //   super();
+  //
+  //   this.state = {
+  //     email: '',
+  //     showError: false,
+  //     messageFromServer: '',
+  //     showNullError: false
+  //   };
+  // }
+  //
+  // handleChange = name => event => {
+  //   this.setState({
+  //     [name]: event.target.value
+  //   });
+  // };
+  //
+  // sendEmail = e => {
+  //   e.preventDefault();
+  //   if (this.state.email === '') {
+  //     this.setState({
+  //       showError: false,
+  //       messageFromServer: '',
+  //       showNullError: true
+  //     });
+  //   } else {
+  //     axios
+  //       .post('http://localhost:3000/forgotPassword', {
+  //         email: this.state.email
+  //       })
+  //       .then(response => {
+  //         console.log(response.data);
+  //         if (response.data === 'email not in db') {
+  //           this.setState({
+  //             showError: true,
+  //             messageFromServer: '',
+  //             showNullError: false
+  //           });
+  //       } else {
+  //           axios
+  //               .post('http://localhost:3003/forgotPassword', {
+  //                   email: this.state.email,
+  //               })
+  //               .then(response => {
+  //                   console.log(response.data);
+  //                   if (response.data === 'email not in db') {
+  //                       this.setState({
+  //                           showError: true,
+  //                           messageFromServer: '',
+  //                           showNullError: false,
+  //                       });
+  //                   } else if (response.data === 'recovery email sent') {
+  //                       this.setState({
+  //                           showError: false,
+  //                           messageFromServer: 'recovery email sent',
+  //                           showNullError: false,
+  //                       });
+  //                   }
+  //               })
+  //               .catch(error => {
+  //                   console.log(error.data);
+  //               });
+  //       }
+  //   };
+  //
+  //   render() {
+  //       const { email, messageFromServer, showNullError, showError } = this.state;
+  //
+  //       return (
+  //           <div>
+  //               <Menu/>
+  //               <HeaderBar title={title} />
+  //               <form className="profile-form" onSubmit={this.sendEmail}>
+  //                   <TextField
+  //                       style={inputStyle}
+  //                       id="email"
+  //                       label="email"
+  //                       value={email}
+  //                       onChange={this.handleChange('email')}
+  //                       placeholder="Email Address"
+  //                   />
+  //                   <SubmitButtons
+  //                       buttonStyle={forgotButton}
+  //                       buttonText={'Send Password Reset Email'}
+  //                   />
+  //               </form>
+  //               {showNullError && (
+  //                   <div>
+  //                       <p>The email address cannot be null.</p>
+  //                   </div>
+  //               )}
+  //               {showError && (
+  //                   <div>
+  //                       <p>
+  //                           That email address isn't recognized. Please try again or register
+  //                           for a new account.
+  //                       </p>
+  //                       <LinkButtons
+  //                           buttonText={`Register`}
+  //                           buttonStyle={registerButton}
+  //                           link={'/register'}
+  //                       />
+  //                   </div>
+  //               )}
+  //               {messageFromServer === 'recovery email sent' && (
+  //                   <div>
+  //                       <h3>Password Reset Email Successfully Sent!</h3>
+  //                   </div>
+  //               )}
+  //               <LinkButtons
+  //                   buttonText={`Go Home`}
+  //                   buttonStyle={homeButton}
+  //                   link={'/'}
+  //               />
+  //           </div>
+  //       );
+  //   }
+  // };
   /*
   render() {
     const { email, messageFromServer, showNullError, showError } = this.state;
