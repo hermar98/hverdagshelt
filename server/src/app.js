a012506b411a47ad27fd61da40f7ca384eea429f
Trond Jacob Rondestvedt <trondjro@stud.ntnu.no>
2019-01-15 14:59:25 +0100

    UserIssue & UserMunicipal tests (backend)

--- server/src/app.js
+++ server/src/app.js
@@ -398,7 +398,8 @@ app.get('/secure/userMun/:id', (req: Request, res: Response) => {
       {
         model: Municipal,
         as: 'Municipals',
-        attributes: ['munId', 'name']
+        attributes: ['munId', 'name'],
+        through: {model: UserMunicipal, as: 'UserMunicipals', attributes:[]}
       }
     ],
     attributes: [],
@@ -425,7 +426,8 @@ app.get('/secure/userIssue/:id', (req: Request, res: Response) => {
       {
         model: Issue,
         as: 'Issues',
-        attributes: ['issueId', 'name']
+        attributes: ['issueId', 'name'],
+        through: {model: UserIssue, as: 'UserIssues', attributes:[]}
       }
     ],
     attributes: [],
@@ -436,7 +438,7 @@ app.get('/secure/userIssue/:id', (req: Request, res: Response) => {
 app.post('/secure/user/:userId/issue/:issueId', (req: Request, res: Response) => {
   return UserIssue.create({
     userId: req.params.userId,
-    munId: req.params.issueId
+    issueId: req.params.issueId
   }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
 });
 
