bab29970375126b70aff5635d6bb434d652f5d98
ChrisAxell <Chris.axell@gmail.com>
2019-01-15 16:32:48 +0100

    Første push for image opplasting

---
+++ server/src/routes/feedback.js
@@ -0,0 +1,57 @@
+import {Feedback} from "../models";
+require('dotenv').config();
+
+type Request = express$Request;
+type Response = express$Response;
+
+const app = require('../app');
+
+app.post('/secure/feedback', (req: Request, res: Response) => {
+    if (!(req.body instanceof Object)) return res.sendStatus(400);
+    return Feedback.create({
+        name: req.body.name,
+        content: req.body.content,
+        issueId: req.body.issueId,
+        userId: req.body.userId
+    }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
+});
+
+app.get('/issues/:id/feedback/:lim/limit/:off/offset/asc', (req: Request, res: Response) => {
+    return Feedback.findAll({
+        where: {
+            issueId: Number(req.params.id)
+        },
+        offset: req.params.offs,
+        limit: req.params.lim,
+        order: [
+            ['createdAt']
+        ]
+        }).then(
+        issue => (issue ? res.send(issue) : res.sendStatus(404))
+    );
+});
+
+app.get('/issues/:id/feedback/:lim/limit/:offs/offset/desc', (req: Request, res: Response) => {
+    return Feedback.findAll({
+        where: {
+                issueId: Number(req.params.id)
+        },
+        offset: req.params.offs,
+        limit: req.params.lim,
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    }).then(
+        issue => (issue ? res.send(issue) : res.sendStatus(404))
+    );
+});
+
+app.get('/feedback/:id', (req: Request, res: Response) => {
+    return Feedback.findOne({
+        where: {
+            feedbackId: Number(req.params.id)
+        }
+    }).then(
+        feedbak => (feedbak ? res.send(feedbak) : res.sendStatus(404))
+    );
+});
