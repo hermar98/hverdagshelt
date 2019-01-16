6fd3d039a88a3150aff1484ae8bb1106f1f16976
hermanrm <hermanrm@stud.ntnu.no>
2019-01-16 13:27:17 +0100

    Fixed menu

--- client/src/components/menu/UserMenu.js
+++ client/src/components/menu/UserMenu.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import {DropdownDivider, DropdownFooter, DropdownHeader, DropdownItem, NavBar} from '../../widgets';
-import {User} from "../../models";
+import {User, Municipal} from "../../models";
 import {history} from "../../index";
 import {tokenManager} from "../../tokenManager";
 
