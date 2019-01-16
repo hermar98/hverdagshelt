9f7eefb407242cd7d4da4b7ee7616f95021102bc
sebasman <sebasman@stud.ntnu.no>
2019-01-16 08:38:46 +0100

    .

--- client/src/components/pages/MunicipalPage.js
+++ client/src/components/pages/MunicipalPage.js
@@ -3,59 +3,61 @@
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { autocomplete, glob } from '../../../public/autocomplete';
-import {eventService, issueService} from '../../services';
+import { eventService, issueService } from '../../services';
 import { Alert, Card } from '../../widgets';
 import { history } from '../../index';
 import { IssueSmall } from '../issueViews/issueViews';
-import { DisplayEvent2 } from "./EventPage";
-import Menu from "../menu/Menu";
+import { DisplayEvent2 } from './EventPage';
+import Menu from '../menu/Menu';
 
-export class MunicipalPage extends Component <{match: {params: {munId: number}}}>{
-    issues = [];
-    events = [];
-    render() {
-        return(
-            <div>
-                <Menu />
-                <div className="row">
-                    <div className="col-lg-6">
-                        <Card title="Feil/mangler">
-                            <ul className="container-fluid">
-                                {this.issues.map(issue =>
-                                    <Card>
-                                        <li key={issue.issueId}>
-                                            <IssueSmall issue={issue}/>
-                                        </li>
-                                    </Card>
-                                )}
-                            </ul>
-                        </Card>
-                    </div>
-                    <div className="col-lg-6">
-                        <ul className="container-fluid">
-                            {this.events.map(event =>
-                                <li key={event.eventId}>
-                                    <DisplayEvent2 event={event}/>
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+export class MunicipalPage extends Component<{ match: { params: { munId: number } } }> {
+  issues = [];
+  events = [];
+  render() {
+    return (
+      <div>
+        <Menu />
+        <div className="row">
+          <div className="col-lg-6">
+            <Card title="Feil/mangler">
+              <ul className="container-fluid">
+                {this.issues.map(issue => (
+                  <Card>
+                    <li key={issue.issueId}>
+                      <IssueSmall issue={issue} />
+                    </li>
+                  </Card>
+                ))}
+              </ul>
+            </Card>
+          </div>
+          <div className="col-lg-6">
+            <ul className="container-fluid">
+              {this.events.map(event => (
+                <li key={event.eventId}>
+                  <DisplayEvent2 event={event} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    mounted() {
-        issueService
-            // .getIssuesByMunicipal(this.props.match.params.munId)
-            .getIssues()
-            .then(issues => {(this.issues = issues);})
-            .catch((error: Error) => Alert.danger(error.message));
+  mounted() {
+    issueService
+      .getIssuesByMunicipal(this.props.match.params.munId)
+      // .getIssues()
+      .then(issues => {
+        this.issues = issues;
+      })
+      .catch((error: Error) => Alert.danger(error.message));
 
-        eventService
-            // .getEventsByMunicipal(this.props.match.params.munId)
-            .getEvents()
-            .then(events => (this.events = events))
-            .catch((error: Error) => Alert.danger(error.message));
-    }
+    eventService
+      .getEventsByMunicipal(this.props.match.params.munId)
+      // .getEvents()
+      .then(events => (this.events = events))
+      .catch((error: Error) => Alert.danger(error.message));
+  }
 }
