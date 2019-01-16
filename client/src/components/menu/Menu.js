e451286c36d79a6b8bc31d8fad9faf4a61252e65
sebasman <sebasman@stud.ntnu.no>
2019-01-16 15:32:58 +0100

    Fixed recover password

--- client/src/components/menu/Menu.js
+++ client/src/components/menu/Menu.js
@@ -23,50 +23,53 @@ export default class Menu extends Component {
   municipal = new Municipal();
   munId = localStorage.getItem('munId');
 
-    render() {
-        if (this.user) {
-            if (this.user.rank === 1) {
-                return (
-                    <NavBar>
-                        <NavBar.Brand image={this.municipal.municipalShield} to={'/municipal/' + this.munId}>{this.municipal.name + ' kommune'}</NavBar.Brand>
-                        <NavBar.Link to={'/municipal/' + this.munId + '/fileIssue'}>Registrer sak</NavBar.Link>
-                        <NavBar.Dropdown title={this.user.firstName + ' ' + this.user.lastName}>
-                            <DropdownHeader>{this.user.email}</DropdownHeader>
-                            <DropdownFooter>Privatperson</DropdownFooter>
-                            <DropdownDivider/>
-                            <DropdownItem onClick={this.toProfile}>Min profil</DropdownItem>
-                            <DropdownItem onClick={this.logout}>Logg ut</DropdownItem>
-                        </NavBar.Dropdown>
-                    </NavBar>
-                );
-            } else if (this.user.rank === 2) {
-                return (
-                    <NavBar>
-                        <NavBar.Brand image={this.municipal.municipalShield} to={'/municipal/' + this.munId}>{this.municipal.name + ' kommune'}</NavBar.Brand>
-                        <NavBar.Link to={'/municipal/' + this.munId + '/fileIssue'}>Registrer sak</NavBar.Link>
-                        <NavBar.Link to={'/municipal/' + this.munId + '/registerEvent'}>Registrer event</NavBar.Link>
-                        <NavBar.Dropdown title={this.user.firstName + ' ' + this.user.lastName}>
-                            <DropdownHeader>{this.user.email}</DropdownHeader>
-                            <DropdownFooter>Kommuneansatt</DropdownFooter>
-                            <DropdownDivider/>
-                            <DropdownItem onClick={this.toProfile}>Min profil</DropdownItem>
-                            <DropdownItem onClick={this.logout}>Logg ut</DropdownItem>
-                        </NavBar.Dropdown>
-                    </NavBar>
-                );
-            }
-        }
+  render() {
+    if (this.user) {
+      if (this.user.rank === 1) {
         return (
-            <NavBar>
-                <NavBar.Brand image={this.municipal.municipalShield}>{this.municipal.name + ' kommune'}</NavBar.Brand>
-                <NavBar.Link to={'/municipal/' + this.munId + '/login'}>Logg inn</NavBar.Link>
-                <NavBar.Link to={'/municipal/' + this.munId + '/register'}>Registrer bruker</NavBar.Link>
-            </NavBar>
+          <NavBar>
+            <NavBar.Brand image={this.municipal.municipalShield} to={'/municipal/' + this.munId}>
+              {this.municipal.name + ' kommune'}
+            </NavBar.Brand>
+            <NavBar.Link to={'/municipal/' + this.munId + '/fileIssue'}>Registrer sak</NavBar.Link>
+            <NavBar.Dropdown title={this.user.firstName + ' ' + this.user.lastName}>
+              <DropdownHeader>{this.user.email}</DropdownHeader>
+              <DropdownFooter>Privatperson</DropdownFooter>
+              <DropdownDivider />
+              <DropdownItem onClick={this.toProfile}>Min profil</DropdownItem>
+              <DropdownItem onClick={this.logout}>Logg ut</DropdownItem>
+            </NavBar.Dropdown>
+          </NavBar>
         );
+      } else if (this.user.rank === 2) {
+        return (
+          <NavBar>
+            <NavBar.Brand image={this.municipal.municipalShield} to={'/municipal/' + this.munId}>
+              {this.municipal.name + ' kommune'}
+            </NavBar.Brand>
+            <NavBar.Link to={'/municipal/' + this.munId + '/fileIssue'}>Registrer sak</NavBar.Link>
+            <NavBar.Link to={'/municipal/' + this.munId + '/registerEvent'}>Registrer event</NavBar.Link>
+            <NavBar.Dropdown title={this.user.firstName + ' ' + this.user.lastName}>
+              <DropdownHeader>{this.user.email}</DropdownHeader>
+              <DropdownFooter>Kommuneansatt</DropdownFooter>
+              <DropdownDivider />
+              <DropdownItem onClick={this.toProfile}>Min profil</DropdownItem>
+              <DropdownItem onClick={this.logout}>Logg ut</DropdownItem>
+            </NavBar.Dropdown>
+          </NavBar>
+        );
+      }
     }
+    return (
+      <NavBar>
+        <NavBar.Brand image={this.municipal.municipalShield}>{this.municipal.name + ' kommune'}</NavBar.Brand>
+        <NavBar.Link to={'/municipal/' + this.munId + '/login'}>Logg inn</NavBar.Link>
+        <NavBar.Link to={'/municipal/' + this.munId + '/register'}>Registrer bruker</NavBar.Link>
+      </NavBar>
+    );
+  }
 
-
-    mounted() {
+  mounted() {
     userService
       .getToken()
       .then(() => {
