14f0b52957886acc11e1a6f4816c4b6b7880efec
jorgaas <jorgaas@stud.ntnu.no>
2019-01-18 10:21:43 +0100

    services.js split into separate classes refactoring to fix broken imports

--- client/tests/serviceTest.js
+++ client/tests/serviceTest.js
@@ -4,8 +4,9 @@ import { eventService } from '../src/services/EventService';
 import { Issue, User, Event } from '../src/models';
 import {userService} from "../src/services/UserService";
 import {issueService} from "../src/services/IssueService";
+import service from "../src/services/Service";
 
-let mock = new MockAdapter(axios);
+let mock = new MockAdapter(service);
 let user = {
   userId: 1,
   firstName: 'Jimmy',
