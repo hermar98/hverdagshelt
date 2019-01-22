a1afb2b8df6ecfff1eb38f5cbd1b975b6a10c727
hermanrm <hermanrm@stud.ntnu.no>
2019-01-22 14:22:46 +0100

    admin page

--- client/src/components/pages/AdminPage.js
+++ client/src/components/pages/AdminPage.js
@@ -8,7 +8,8 @@ import { userService } from '../../services/UserService.js';
 
 export class AdminPage extends Component {
     users = [];
-    filterRank = null;
+    filterRank = 0;
+    sortDetails = {sortBy: 'userId', order: 'asc'};
 
     render() {
         return (
@@ -17,50 +18,61 @@ export class AdminPage extends Component {
                 <div className="card m-3">
                     <div className="card-body">
                         <h2 className="card-title">Administrer brukere</h2>
+                        <Button.Success OnClick={() => console.log('d')}>Hei</Button.Success>
                         <div className="col-sm-3 my-3">
-                            <select className="form-control">
-                                <option>Alle typer brukere</option>
-                                <option>Privatperson</option>
-                                <option>Bedrift</option>
-                                <option>Kommuneansatt</option>
-                                <option>Admin</option>
+                            <select className="form-control" onChange={event => this.filterRank = event.target.value}>
+                                <option value={0}>Alle typer brukere</option>
+                                <option value={1}>Privatperson</option>
+                                <option value={2}>Bedrift</option>
+                                <option value={3}>Kommuneansatt</option>
+                                <option value={4}>Admin</option>
                             </select>
                         </div>
-                        <table className="table table-hover">
-                            <thead>
+                        <div className="col-sm-3 my-3">
+                            <select className="form-control">
+                                <option value={0}>Alle kommuner</option>
+                                <option value={1}>Fredrikstad</option>
+                                <option value={2}>Bedrift</option>
+                                <option value={3}>Kommuneansatt</option>
+                                <option value={4}>Admin</option>
+                            </select>
+                        </div>
+                        <table className="table table-bordered table-hover">
+                            <thead id="t-head">
                                 <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Fornavn</th>
-                                    <th scope="col">Etternavn</th>
-                                    <th scope="col">E-post</th>
-                                    <th scope="col">Type bruker</th>
-                                    <th scope="col">Kommune</th>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
+                                    <th scope="col" onClick={() => this.sort('userId')}>
+                                        ID {this.getArrow('userId')}
+                                    </th>
+                                    <th scope="col" onClick={() => this.sort('firstName')}>
+                                        Fornavn {this.getArrow('firstName')}
+                                    </th>
+                                    <th scope="col" onClick={() => this.sort('lastName')}>
+                                        Etternavn {this.getArrow('lastName')}
+                                    </th>
+                                    <th scope="col" onClick={() => this.sort('email')}>
+                                        E-post {this.getArrow('email')}
+                                    </th>
+                                    <th scope="col" onClick={() => this.sort('rank')}>
+                                        Type bruker {this.getArrow('rank')}
+                                    </th>
+                                    <th scope="col" onClick={() => this.sort('munId')}>
+                                        Kommune {this.getArrow('munId')}
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.filterRank ? (
-                                    this.users.map(user => (
-                                        <tr>
-                                            <th scope="row">{user.userId}</th>
-                                            <td>{user.firstName}</td>
-                                            <td>{user.lastName}</td>
-                                            <td>{user.email}</td>
-                                            <td>{user.rank}</td>
-                                            <td>{user.munId ? (user.munId) : (' - ')}</td>
-                                        </tr>
-                                    ))) : (
-                                    this.users.filter(user => user.rank === this.filterRank).map(user => (
-                                        <tr>
-                                            <th scope="row">{user.userId}</th>
-                                            <td>{user.firstName}</td>
-                                            <td>{user.lastName}</td>
-                                            <td>{user.email}</td>
-                                            <td>{user.rank}</td>
-                                            <td>{user.munId ? (user.munId) : (' - ')}</td>
-                                        </tr>
-                                    )))}
+                                {this.users.filter(user => {
+                                    return user.rank == this.filterRank || this.filterRank == 0;
+                                }).map(user => (
+                                    <tr key={user.userId}>
+                                        <th scope="row">{user.userId}</th>
+                                        <td>{user.firstName}</td>
+                                        <td>{user.lastName}</td>
+                                        <td>{user.email}</td>
+                                        <td>{this.getRankName(user.rank)}</td>
+                                        <td>{user.munId ? (user.munId) : (' - ')}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
@@ -71,20 +83,70 @@ export class AdminPage extends Component {
 
     mounted() {
         userService.getUsers()
-            .then(users => {
-                this.users = users.map(user => {
-                    if (user.rank === 1) {
-                        user.rank = 'Privatperson';
-                    } else if (user.rank === 2) {
-                        user.rank = 'Bedrift';
-                    } else if (user.rank === 3) {
-                        user.rank = 'Kommuneansatt';
-                    } else if (user.rank === 4) {
-                        user.rank = 'Admin';
-                    }
-                    return user;
-                })
-            })
+            .then(users => this.users = users)
             .catch((error: Error) => Alert.danger(error.message));
+    }
+
+    getRankName(rank: number): string {
+        if (rank === 1) {
+            return 'Privatperson';
+        } else if (rank === 2) {
+            return 'Bedrift';
+        } else if (rank === 3) {
+            return 'Kommuneansatt';
+        } else if (rank === 4) {
+            return 'Admin';
+        }
+    }
+
+    sort(field: string) {
+        if (this.sortDetails.sortBy === field) {
+            this.users.reverse();
+            if (this.sortDetails.order === 'asc') {
+                this.sortDetails.order = 'desc';
+            } else if (this.sortDetails.order === 'desc') {
+                this.sortDetails.order = 'asc';
+            }
+        } else {
+            if (field === 'userId') {
+                this.users.sort(function(a, b) {
+                    return a.userId - b.userId;
+                });
+            } else if (field === 'firstName') {
+                this.users.sort(function(a, b) {
+                    return a.firstName.localeCompare(b.firstName);
+                });
+            } else if (field === 'lastName') {
+                this.users.sort(function(a, b) {
+                    return a.lastName.localeCompare(b.lastName);
+                });
+            } else if (field === 'email') {
+                this.users.sort(function(a, b) {
+                    return a.email.localeCompare(b.email);
+                });
+            } else if (field === 'rank') {
+                this.users.sort(function(a, b) {
+                    return a.rank - b.rank;
+                });
+            } else if (field === 'munId') {
+                this.users.sort(function(a, b) {
+                    return a.munId - b.munId;
+                });
+            }
+            this.sortDetails.sortBy = field;
+            this.sortDetails.order = 'asc';
+        }
+    }
+
+    getArrow(field: string) {
+        if (this.sortDetails.sortBy === field) {
+            if (this.sortDetails.order === 'asc') {
+                return <div className="float-right">▲</div>
+            } else {
+                return <div className="float-right">▼</div>
+            }
+        } else {
+            return <div/>;
+        }
     }
 }
