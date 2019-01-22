6ba1468b120c2664fc8480ffad2e2fb453fff5b6
Sander Nicolausson <sandern@stud.ntnu.no>
2019-01-22 14:14:10 +0100

    Terms of Service finished

--- client/src/components/pages/TermsOfServicePage.js
+++ client/src/components/pages/TermsOfServicePage.js
@@ -2,22 +2,24 @@
 
 import * as React from 'react';
 import { Component } from 'react-simplified';
+import {NewMenu} from '../../components/menu/NewMenu';
 
 export class TermsOfService extends Component {
   render() {
     return(
       <div id="content" className="one-column">
-        <div id="main">
-          <h1>Vilkår for bruk</h1>
-          <h2>Innledning</h2>
+        <NewMenu />
+        <div className="TOSMain">
+          <h1 className="TOSH1">Vilkår for bruk</h1>
+          <h2 className="TOSH2">Innledning</h2>
 
-          <p>Dette nettstedet («nettstedet») styres av Hverdagshelt AS. Dette gjøres tilgjengelig for deg i
+          <p className="TOSp">Dette nettstedet («nettstedet») eies av Frida Magnarsdatter Lund styres av Hverdagshelt AS. Dette gjøres tilgjengelig for deg i
             den hensikt å informere. Ved å besøke eller bruke dette nettstedet, er du enig i disse vilkårene for bruk.
             Hvis du ikke er enig i disse vilkårene for bruk, skal du ikke bruke nettstedet.</p>
 
-          <h2>Din bruk av nettstedet</h2>
+          <h2 className="TOSH2">Din bruk av nettstedet</h2>
 
-          <p>Dette nettstedet er ment for din personlige og ikke-kommersielle bruk dersom annet ikke er spesifisert.
+          <p className="TOSp">Dette nettstedet er ment for din personlige og ikke-kommersielle bruk dersom annet ikke er spesifisert.
             Du kan ikke endre, kopiere, distribuere, gjøre tilgjengelig, sende, vise, yte, gjengi, publisere,
             lisensiere, lage etterligninger av, overføre eller selge informasjon, programvare, produkter eller
             tjenester du har fått fra dette nettstedet. Du kan vise eller kopiere elektronisk, laste ned og skrive ut
@@ -27,7 +29,7 @@ export class TermsOfService extends Component {
             distribuering, gjengivelse, visning eller overføring av innholdet på dette nettstedet uten skriftlig
             samtykke på forhånd, er forbudt.</p>
 
-            <p>Alt materiale du laster ned, inkludert, men ikke begrenset til programvare, filer, illustrasjoner,
+            <p className="TOSp">Alt materiale du laster ned, inkludert, men ikke begrenset til programvare, filer, illustrasjoner,
             data eller hvilket som helst annet innhold, eies av oss eller våre lisensinnehavere. Når du laster
             ned dette materialet, er det gitt til deg av oss under en lisens som kan tilbakekalles. Vi beholder helt
             og fullstendig eiendomsretten til programvaren og til alle de tilhørende åndsverksrettighetene. Du kan
@@ -35,9 +37,9 @@ export class TermsOfService extends Component {
             eller på annen måte konvertere det til en hvilken som helst annen form. Du kan ikke opprette en
             hyperkobling til et annet webområde.</p>
 
-          <h2>Materiale som du leverer</h2>
+          <h2 className="TOSH2">Materiale som du leverer</h2>
 
-          <p>Du vedstår at du er ansvarlig for alle opplastinger du gjør, inkludert lovlighet, pålitelighet, egnethet,
+          <p className="TOSp">Du vedstår at du er ansvarlig for alle opplastinger du gjør, inkludert lovlighet, pålitelighet, egnethet,
             originalitet og opphavsrett til alt slikt materiale. På dette nettstedet kan du ikke laste opp, distribuere
             eller på annen måte publisere et hvilket som helst innhold som er injurierende, ærekrenkende, uanstendig,
             truende, krenkende for personvern og publiseringsrettigheter, støtende, ulovlig eller på annen måte
@@ -45,7 +47,7 @@ export class TermsOfService extends Component {
             hvilken som helst part eller på annen måte forårsake ansvar eller overtredelse av en hvilken som helst lov.
             Du kan ikke laste opp kommersielt innhold på dette nettstedet.</p>
 
-            <p>Bortsett fra all personopplysninger vi får fra deg i samsvar med vår
+            <p className="TOSp">Bortsett fra all personopplysninger vi får fra deg i samsvar med vår
             Personvernmeddelelse, vil alle bemerkninger, forslag, ideer, illustrasjoner eller
             annen informasjon som du kommuniserer til oss via dette nettstedet, bli vår eiendom,
             selv om denne avtalen senere blir sagt opp. Vi og våre utpekte kan fritt kopiere, fremlegge,
@@ -53,9 +55,9 @@ export class TermsOfService extends Component {
             og alle kommersielle og ikke-kommersielle formål uten en forpliktelse til å gi deg eller andre
             kompensasjon for materialet.</p>
 
-          <h2>Ansvarsfraskrivelse</h2>
+          <h2 className="TOSH2">Ansvarsfraskrivelse</h2>
 
-          <p>Vi gir ingen garantier eller er ikke ansvarlig for nøyaktigheten eller fullstendigheten til innholdet
+          <p className="TOSp">Vi gir ingen garantier eller er ikke ansvarlig for nøyaktigheten eller fullstendigheten til innholdet
             på nettstedet. Dette nettstedet og materialet, informasjonen, tjenestene og produktene på nettstedet,
             inkludert, uten begrensning, tekst, illustrasjoner og lenker, er gitt «som det er» og uten noen som helst
             garantier, verken uttrykkelige eller underforståtte. I den utstrekning det er tillatt i henhold til
@@ -69,6 +71,30 @@ export class TermsOfService extends Component {
             fullstendighet, riktighet, nøyaktighet, egnethet, nytte, aktualitet, pålitelighet eller annet. I
             tillegg til det ovennevnte og i den grad det tillates i henhold til gjeldende lov, påtar du (og ikke oss)
             deg ansvaret for alle kostnader til all nødvendig service, reparasjon eller rettelse.</p>
+
+          <h2 className="TOSH2">Erstatning</h2>
+
+          <p className="TOSp">Du går med på å betale erstatning, forsvare og holde oss, våre ledere, direktører, ansatte, agenter,
+            lisenshavere og leverandører skadesløse fra og mot alle tap, utgifter, skader og omkostninger,
+            inkludert rimelige saksomkostninger, som skyldes en hvilken som helst overtredelse av vilkårene for
+            bruk eller en hvilken som helst aktivitet knyttet til din Internett-konto (inkludert, men ikke begrenset
+            til uaktsom eller urettmessig bruk), av deg eller en hvilken som helst annen person som får tilgang til
+            nettstedet ved bruk av din Internett-konto.</p>
+
+          <h2 className="TOSH2">Oppsigelse av avtalen</h2>
+
+          <p className="TOSp">Denne avtalen er gjeldende frem til den blir sagt opp av en av partene. Du kan si opp avtalen til
+            enhver tid ved å destruere alt materiale du har fått fra dette nettstedet sammen med all relatert
+            dokumentasjon og alle kopier og installasjoner. Vi kan si opp denne avtalen til enhver tid og uten
+            å gi deg varsel på forhånd hvis, etter vårt skjønn, du bryter et hvilket som helst vilkår eller en
+            betingelse i denne avtalen. Ved oppsigelse skal du destruere alt materiale og opphøre tilgangen til
+            nettstedet.</p>
+
+          <h2 className="TOSH2">Diverse</h2>
+
+          <p className="TOSp">Hvis en bestemmelse i denne avtalen er ulovlig, ugyldig eller ikke kan håndheves, vil de andre
+            bestemmelsene i avtalen forbli gjeldende. Dette er den fullstendige avtalen mellom deg og oss som
+            angår alle aspekter ved din bruk av dette nettstedet.</p>
         </div>
       </div>
     )
