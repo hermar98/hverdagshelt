d86f84ff3b54936a75446642a302d2e2cca25507
Herman Christiansen <hermanchristiansen@dhcp-10-22-154-221.wlan.ntnu.no>
2019-01-24 09:23:45 +0100

    Final fucking navbar hopefully

--- client/src/components/pages/FileIssuePage.js
+++ client/src/components/pages/FileIssuePage.js
@@ -4,14 +4,12 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import { User } from '../../models/User.js';
 import FileIssueForm from '../../components/forms/FileIssueForm';
-import { RegisterIssueMenu } from '../../components/menu/RegisterIssueMenu.js';
 
 export class FileIssuePage extends Component {
   user = new User();
   render() {
     return (
       <div>
-        <RegisterIssueMenu />
         <FileIssueForm />
       </div>
     );
