7f9b40eb9f3f17f2ed6ff71c22fd5504f9ba30af
hermanrm <hermanrm@stud.ntnu.no>
2019-01-25 18:34:37 +0100

    Fixed redirect

--- server/src/ModelsTestData.js
+++ server/src/ModelsTestData.js
@@ -647,7 +647,7 @@ class ModelsTestData {
           userId: 1,
           firstName: 'Privat',
           lastName: 'Person',
-          email: 'sander-nico@hotmail.com',
+          email: 'martinsenhr@gmail.com',
           rank: 1,
           salt: 'a83f4da094cc247b',
           hashStr:
